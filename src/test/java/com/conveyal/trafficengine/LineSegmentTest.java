1984655c72333e0584405eb83b2ae530eb1cfba1
Brandon Martin-Anderson <badhill@gmail.com>
2015-03-24 15:13:37 -0700

    line segment test

---
+++ src/test/java/com/conveyal/trafficengine/LineSegmentTest.java
@@ -0,0 +1,17 @@
+package com.conveyal.trafficengine;
+
+import com.vividsolutions.jts.geom.Coordinate;
+
+import junit.framework.TestCase;
+
+public class LineSegmentTest extends TestCase {
+
+	public void testBasic(){
+		LineSegment ls1 = new LineSegment(new Coordinate(0,0), new Coordinate(4,0));
+		LineSegment ls2 = new LineSegment(new Coordinate(1,-1), new Coordinate(1,1));
+		
+		assertEquals( 0.25, ls1.intersectionDistance( ls2 ) );
+		
+		assertEquals( 0.5, ls2.intersectionDistance( ls1 ) );
+	}
+}
