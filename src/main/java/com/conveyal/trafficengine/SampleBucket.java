44f6dc23bde9ef109153ced653b0c924bc6637fb
Brandon Martin-Anderson <badhill@gmail.com>
2015-03-27 12:22:04 -0700

    store speed histograms

--- src/main/java/com/conveyal/trafficengine/SampleBucket.java
+++ src/main/java/com/conveyal/trafficengine/SampleBucket.java
@@ -3,9 +3,13 @@ package com.conveyal.trafficengine;
 import java.io.Serializable;
 
 public class SampleBucket implements Serializable{
+	int TOP_SPEED = 35;
+	double BUCKET_WIDTH = 0.5;
+	
 	private static final long serialVersionUID = 2703516691932416832L;
 	public int count;
 	public double mean;
+	public int[] buckets = new int[(int)(TOP_SPEED/BUCKET_WIDTH)+1];
 	
 	SampleBucket(){
 		count=0;
@@ -15,6 +19,11 @@ public class SampleBucket implements Serializable{
 	public void update(SpeedSample ss) {
 		mean = (mean*count + ss.speed)/(count+1);
 		count += 1;
+		
+		if( ss.speed < TOP_SPEED ){
+			int bucket = (int)(ss.speed/BUCKET_WIDTH);
+			buckets[bucket] += 1;
+		}
 	}
 	
 	public String toString(){
